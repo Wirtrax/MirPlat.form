bba4aafd62271a7f7c9facfff5e33bdf8eecb29a
Wirtrax <ngrbuster@gmail.com>
2026-07-25 15:25:59 +0300

    feat: environment is configured

--- src/Frontend/eslint.config.js
+++ src/Frontend/eslint.config.js
@@ -1,22 +1,43 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-plugin-prettier';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
   globalIgnores(['dist']),
+
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      ...tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
+
+    plugins: {
+      prettier,
+    },
+
     languageOptions: {
       globals: globals.browser,
     },
+
+    rules: {
+      'prettier/prettier': 'error',
+
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+      indent: ['error', 2],
+      'comma-dangle': ['error', 'only-multiline'],
+      'arrow-parens': ['error', 'always'],
+      'object-curly-spacing': ['error', 'always'],
+    },
   },
-])
+
+  eslintConfigPrettier,
+]);
