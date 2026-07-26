feeae5cd3c9bcc478e371d07b947f97a73833ae0
millipo <palina517@gmail.com>
2026-07-26 16:18:24 +0300

    feat: add header and update footer styles

--- src/Frontend/src/routes/Root.tsx
+++ src/Frontend/src/routes/Root.tsx
@@ -1,10 +1,12 @@
 import s from './Root.module.scss'
 import { Outlet } from 'react-router-dom'
 import Footer from '../components/Footer/Footer'
+import Header from '../components/Header/Header'
 
 export default function Root() {
     return (
         <div className={s.container}>
+            <Header />
             <Outlet />
             <Footer />
         </div>
