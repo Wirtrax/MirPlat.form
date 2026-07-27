5d45985c9386c45674f06db6ae2bcbfe91cae132
TNMYP <timur.garaev2007@gmail.com>
2026-07-27 18:28:23 +0300

    [BE] rewrited database connection using better nestjs/typeorm module

--- src/Backend/src/app.module.ts
+++ src/Backend/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+        type: 'postgres',
+        host: process.env.DBHOST,
+        port: parseInt(process.env.DBPORT as string),
+        username: process.env.DBUSERNAME,
+        password: process.env.DBPASSWORD,
+        database: process.env.DBNAME,
+        entities: [__dirname + '/entities/*.entity.ts'],
+        synchronize: true, // TODO: Remove this before release to production
+})],
   controllers: [AppController],
   providers: [AppService],
 })
