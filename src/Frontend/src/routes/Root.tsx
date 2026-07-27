8d927b7cfd489e3ead8d3a78eff414e184976b1a
millipo <palina517@gmail.com>
2026-07-27 18:32:17 +0300

    style: update footer and root styles

--- src/Frontend/src/routes/Root.tsx
+++ src/Frontend/src/routes/Root.tsx
@@ -1,14 +1,15 @@
-import s from './Root.module.scss'
 import { Outlet } from 'react-router-dom'
 import Footer from '../components/Footer/Footer'
 import Header from '../components/Header/Header'
 
 export default function Root() {
     return (
-        <div className={s.container}>
+        <>
             <Header />
-            <Outlet />
+            <div className={'container'}>
+                <Outlet />
+            </div>
             <Footer />
-        </div>
+        </>
     )
 }
