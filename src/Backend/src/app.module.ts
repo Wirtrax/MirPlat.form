7a1e25c7d8547413b151bd5f403aaf8f31273bde
TNMYP <timur.garaev2007@gmail.com>
2026-07-27 21:00:33 +0300

    [BE] wadded nestjs/config and enabled configuration with .env file

--- src/Backend/src/app.module.ts
+++ src/Backend/src/app.module.ts
@@ -2,19 +2,23 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-        type: 'postgres',
-        host: process.env.DBHOST,
-        port: parseInt(process.env.DBPORT as string),
-        username: process.env.DBUSERNAME,
-        password: process.env.DBPASSWORD,
-        database: process.env.DBNAME,
-        entities: [__dirname + '/entities/*.entity.ts'],
-        synchronize: true, // TODO: Remove this before release to production
-})],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DBHOST,
+      port: parseInt(process.env.DBPORT as string),
+      username: process.env.DBUSERNAME,
+      password: process.env.DBPASSWORD,
+      database: process.env.DBNAME,
+      entities: [__dirname + '/entities/*.entity.ts'],
+      synchronize: true, // TODO: Remove this before release to production
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
