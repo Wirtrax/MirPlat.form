282e51b7392673c91b83cb3a0b5be5e7f884c5c6
Wirtrax <ngrbuster@gmail.com>
2026-07-27 22:24:17 +0300

    fix: substrate has been modified

--- src/Frontend/src/components/Profile/ProfileInfoCard/ProfileInfoCard.tsx
+++ src/Frontend/src/components/Profile/ProfileInfoCard/ProfileInfoCard.tsx
@@ -1,25 +1,24 @@
-import s from './ProfileInfoCard.module.scss'
-import type { ProfileInfoCardProps } from './ProfileInfoCard.types'
-
+import Substrate from '../../Substrate/Substrate';
+import s from './ProfileInfoCard.module.scss';
+import type { ProfileInfoCardProps } from './ProfileInfoCard.types';
 
 export default function ProfileInfoCard({ title, items }: ProfileInfoCardProps) {
-    return (
-        <div className={s['card']}>
-            <h3 className={s['card-title']}>{title}</h3>
-            <div className={s['cardDetails']}>
-                {
-                    items.map(item => (
-                        <div className={s.item} key={item.id}>
-                            <div className={s.iconWrapper}>{item.icon}</div>
-                            <div className={s.details}>
-                                <span className={s.label}>{item.label}</span>
-                                <span className={s.value}>{item.value}</span>
-                            </div>
-                        </div>
-                    ))
-                }
+  return (
+    <Substrate>
+      <div className={s['card']}>
+        <h3 className={s['card-title']}>{title}</h3>
+        <div className={s['cardDetails']}>
+          {items.map((item) => (
+            <div className={s.item} key={item.id}>
+              <div className={s.iconWrapper}>{item.icon}</div>
+              <div className={s.details}>
+                <span className={s.label}>{item.label}</span>
+                <span className={s.value}>{item.value}</span>
+              </div>
             </div>
-
+          ))}
         </div>
-    )
+      </div>
+    </Substrate>
+  );
 }
--- src/Frontend/src/routes/Root.tsx
+++ src/Frontend/src/routes/Root.tsx
@@ -1,16 +1,19 @@
-import { Outlet } from 'react-router-dom'
-import Footer from '../components/Footer/Footer'
-import Header from '../components/Header/Header'
-import RadioList from '../components/UI/RadioList/RadioList'
+import { Outlet } from 'react-router-dom';
+import Footer from '../components/Footer/Footer';
+import Header from '../components/Header/Header';
+import RadioList from '../components/UI/RadioList/RadioList';
+import Background from '../components/Background/Background';
 
 export default function Root() {
-    return (
-        <>
-            <Header />
-            <div className={'container'}>
-                <Outlet />
-            </div>
-            <Footer />
-        </>
-    )
-  }
+  return (
+    <>
+      <Background>
+        <Header />
+        <div className={'container'}>
+          <Outlet />
+        </div>
+        <Footer />
+      </Background>
+    </>
+  );
+}
