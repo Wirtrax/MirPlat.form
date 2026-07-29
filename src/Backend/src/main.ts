bf73f68274083458b1080d9328466160d63154b0
TNMYP <timur.garaev2007@gmail.com>
2026-07-29 22:34:41 +0300

    added swagger integration

--- src/Backend/src/main.ts
+++ src/Backend/src/main.ts
@@ -1,12 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
     transform: true
   }))
+  const config = new DocumentBuilder()
+    .setTitle('MirPlat.form API')
+    .build();
+  SwaggerModule.setup('api', app, SwaggerModule.createDocument(app, config))
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
