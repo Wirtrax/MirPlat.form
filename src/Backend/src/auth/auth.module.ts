511ac29d19f6d794594ec14b7228021eca7a0d04
TNMYP <timur.garaev2007@gmail.com>
2026-07-30 18:48:44 +0300

    [BE][Auth] added jwt guard

--- src/Backend/src/auth/auth.module.ts
+++ src/Backend/src/auth/auth.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { UserService } from 'src/user/user.service';
+import { JwtGuard } from './jwt.guard';
 
 @Module({
-imports:[UserService]
+imports:[
+  UserService,
+  JwtModule.register({
+    global: true,
+    secret: process.env.JWTSECRET,
+    signOptions: { expiresIn: '10m' }
+  })],
+  providers: [JwtGuard],
+  exports: [JwtGuard]
 })
 export class AuthModule {}
