f27468e5bd5101ba1f5ab4c5c2a436e5896e85f5
Artyom <zujkov.ks52@gmail.com>
2026-08-02 17:06:13 +0300

    fix: add data to entity

---
+++ src/Backend/src/entities/admins.entity.ts
@@ -0,0 +1,22 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+
+export enum AdminRole {
+  ADMIN = 'admin',
+  SUPER_ADMIN = 'super_admin',
+  DECLINED = 'declined',
+}
+
+@Entity()
+export class Admin {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ unique: true })
+    login: string;
+
+    @Column()
+    password_hash: string;
+
+    @Column({ type: 'enum', enum: AdminRole, default: AdminRole.ADMIN })
+    role: AdminRole;
+}
