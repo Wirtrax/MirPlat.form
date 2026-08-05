f0d5e56dc8b739f910b940bd8e003886438af453
millipo <palina517@gmail.com>
2026-08-05 13:41:05 +0300

    fix: ignore leading 8 in phone mask

--- src/Frontend/src/components/UI/Input/Input.tsx
+++ src/Frontend/src/components/UI/Input/Input.tsx
@@ -12,12 +12,19 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 }
 
 export default function Input({ className, label, error, mask, ...props }: InputProps) {
+
   return (
     <div className={s.inputField}>
       {label && <label className={s.label}>{label}</label>}
       {mask ? (
         <IMaskInput
           mask={mask}
+          prepare={(appended: string, masked: any) => {
+            if (mask === '+7 000-000-00-00' && appended === '8' && masked.value === '') {
+              return '';
+            }
+            return appended;
+          }}
           className={clsx(s.input, className)}
           value={props.value}
           name={props.name}
