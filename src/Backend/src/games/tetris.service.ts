7a024f9c05ccf62c8a09e2a3856992933ac7bce3
VlaDragonborn <v.d.selifanov@gmail.com>
2026-08-17 13:21:41 +0300

    [be] tetris.entity and tetris.service bug_fix

--- src/Backend/src/games/tetris.service.ts
+++ src/Backend/src/games/tetris.service.ts
@@ -5,9 +5,6 @@ import { Tetris } from 'src/entities/tetris.entity';
 import { Activity  } from 'src/entities/activity.entity';
 import { Attempt, AttemptStatus } from 'src/entities/attempt.entity';
 
-const PHOTO_LINK_INDEX = 1;
-const TETRIS_REWARD = 10;
-
 @Injectable()
 export class TetrisService {
     constructor (
@@ -16,17 +13,27 @@ export class TetrisService {
         @InjectRepository(Activity)
         private activityRepository: Repository<Activity>,
         @InjectRepository(Attempt)
-        private attemptRepository: Repository<Attempt>
+        private attemptRepository: Repository<Attempt>,
     ) {}
 
-    async getLinkOfTetrisReference(): Promise<string> {
+    async findTetrisRefByName(name: string = "tetris"): Promise<Tetris> {
         const tetris = await this.tetrisRepository.findOneBy({ 
-            id: PHOTO_LINK_INDEX
-            //where: {name: "reference"}
+            name: name,
         });
 
         if(!tetris) {
             throw new NotFoundException('Tetris reference not found');
+        }
+        
+        return tetris      
+    }
+
+    async getLinkOfTetrisReference(): Promise<string> {
+        const tetris = await this.findTetrisRefByName();
+        const link = tetris.photo_example_link;
+
+        if(!link) {
+            throw new NotFoundException('Reference link not found')
         }
 
         return tetris.photo_example_link;
@@ -36,22 +43,21 @@ export class TetrisService {
         user_id: number,
         photo_link: string,
     ): Promise<void> {
-        const attempt = new Attempt();
+        const tetris = await this.findTetrisRefByName()
+        const activity = await this.activityRepository.findOneBy({ name: 'tetris' });
 
-        const tetris = await this.activityRepository.findOneBy({ name: 'tetris' });
-
-        if (!tetris) {
+        if (!activity) {
             throw new Error('Activity "tetris" not found');
         }
 
-        attempt.user.id = user_id;
-        attempt.activity.id = tetris?.id;
-        attempt.is_photo = true;
-        attempt.photo = photo_link;
-        attempt.status = AttemptStatus.WAITING;
-        attempt.reward = TETRIS_REWARD;
-        attempt.created_at = new Date();
-
-        await this.attemptRepository.save(attempt);
+        const attempt = this.attemptRepository.create({
+            user: { id: user_id },
+            activity: { id: activity.id },
+            is_photo: true,
+            photo: photo_link,
+            status: AttemptStatus.WAITING,
+            reward: tetris.reward,
+            created_at: new Date(),
+        });
     }
 }
