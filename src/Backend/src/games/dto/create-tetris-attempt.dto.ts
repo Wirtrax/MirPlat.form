e6f9cc371f24175c6c7520d7845a920362497ebc
Artyom <zujkov.ks52@gmail.com>
2026-08-17 14:23:47 +0300

    change names and path

--- src/Backend/src/games/dto/create-tetris-attempt.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsUrl, IsNotEmpty } from 'class-validator';
-
-export class CreateTetrisAttemptDto {
-    @IsUrl({}, { message: 'photo_link must be correct URL' })
-    @IsNotEmpty({ message: 'photo_link is missing' })
-    photo_link: string;
-}
