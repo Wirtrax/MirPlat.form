e98bae3a679b86a80684ac2a6786d9866b46045b
Artyom <zujkov.ks52@gmail.com>
2026-08-18 18:48:19 +0300

    change endpoint name

--- src/Backend/src/activities/quiz/quiz.controller.ts
+++ src/Backend/src/activities/quiz/quiz.controller.ts
@@ -21,7 +21,7 @@ export class QuizController {
     }
 
     @JWTAuth()
-    @Get('quiz')
+    @Get('quiz/status')
     checkAttempt(@Req() request){
         return this.quizService.checkOnReply(request['userId']);
     }
