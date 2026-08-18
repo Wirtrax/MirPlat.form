68208ad40015a3fb8649d9bc109372d6ae8a64ca
Artyom <zujkov.ks52@gmail.com>
2026-08-18 15:49:34 +0300

    feat: add CURD quiz activity

--- src/Backend/src/activities/quiz/quiz.controller.ts
+++ src/Backend/src/activities/quiz/quiz.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, ParseIntPipe } from '@nestjs/common';
+import { QuizService } from './quiz.service';
+import { JWTAuth } from 'src/auth/jwt.decorator';
+import { Param, Post,} from '@nestjs/common';
+import { Req } from '@nestjs/common';
 
-@Controller('quiz')
-export class QuizController {}
+@Controller('activities')
+export class QuizController {
+    constructor(private readonly quizService: QuizService){}
+    
+    @JWTAuth()
+    @Post('quiz')
+    sendReward(@Req() request) {
+        const userId = request['userId'];
+        return this.quizService.sendReward(userId);
+}
+}
