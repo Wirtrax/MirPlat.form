580173685535d1ddec7f2346266317e5a8c1d735
VlaDragonborn <v.d.selifanov@gmail.com>
2026-08-19 18:46:57 +0300

    [be] add 'api/' in activities endoints

--- src/Backend/src/activities/tetris/tetris.controller.ts
+++ src/Backend/src/activities/tetris/tetris.controller.ts
@@ -24,7 +24,7 @@ export class TetrisController {
     }
 
     @JWTAuth()
-    @Post('tetris')
+    @Post('api/tetris')
     async postTetrisSolution(
         @Req() req: Request,
         @Body() createTetrisAttemptDto: CreateTetrisAttemptDto,
