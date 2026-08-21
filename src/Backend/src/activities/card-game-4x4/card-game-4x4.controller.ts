df5f8f3a2613fd3e05c6311a797112e638b861fd
VlaDragonborn <v.d.selifanov@gmail.com>
2026-08-21 21:22:55 +0300

    [be] Created card-game-4x4 module

---
+++ src/Backend/src/activities/card-game-4x4/card-game-4x4.controller.ts
@@ -0,0 +1,55 @@
+import { Body, Controller, Injectable, Post, Get, Req, BadRequestException, InternalServerErrorException } from "@nestjs/common";
+import { JWTAuth } from "src/auth/jwt.decorator";
+import { CardGame4x4Service } from "./card-game-4x4.service";
+import { CardGameResultDto } from "./dto/card-game-result.dto";
+
+@Controller('api/activities')
+export class CardGame4x4Controller {
+    constructor(
+        private cardGame4x4Service: CardGame4x4Service,
+    ) {}
+
+    @JWTAuth()
+    @Get('card_game_4x4')
+    async getCardGroup() {
+        try {
+            return this.cardGame4x4Service.getCardGroups()
+        } catch(error) {
+            throw new InternalServerErrorException('Unexpected error durin getCardGroup')
+        }
+    }
+
+    @JWTAuth()
+    @Post('card_game_4x4_status')
+    async checkAttempt(@Req() request) {
+        return this.cardGame4x4Service.checkOnReplyCardGame4x4(request['userId'])
+    }
+
+    @JWTAuth()
+    @Post('card_game_4x4')
+    async checkAnswers(
+        @Body() cardGameResultDto: CardGameResultDto,
+        @Req() request,
+    ) {
+        const userId = request['userId'];
+        const count_of_guesed_group = cardGameResultDto.count_of_guesed_group;
+
+        if (typeof userId !== 'number' || isNaN(userId)) {
+            throw new BadRequestException('Invalid user ID');
+        }
+
+        if(count_of_guesed_group < 0) {
+            throw new BadRequestException('Invalid count_of_guessed_group')
+        }
+
+        try {
+            const reward = await this.cardGame4x4Service.sendReward(userId, count_of_guesed_group)
+
+            return {
+                reward: reward,
+            }
+        } catch(error) {
+            throw error
+        }
+    }
+} 
