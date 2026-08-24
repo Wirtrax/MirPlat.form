a0c435ebf2ce4f196dfb36e165eedf250af27f93
Artyom <zujkov.ks52@gmail.com>
2026-08-24 21:42:08 +0300

    feat: add attempt CRUD

--- src/Backend/src/admin-panel/attempts/attempts.controller.ts
+++ src/Backend/src/admin-panel/attempts/attempts.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseIntPipe, Patch, UseGuards } from '@nestjs/common';
 import { AttemptsService } from './attempts.service';
 import { JwtGuard } from '../auth/guards/jwt.guard';
 import { RolesGuard } from '../auth/guards/roles.guard';
+import { AttemptStatus } from 'src/entities/attempt.entity';
 
 @Controller('attempts')
 export class AttemptsController {
@@ -11,5 +12,20 @@ export class AttemptsController {
     @Get()
     getAllAttempts() {
         return this.attemptsService.getAllAttempts();
+    }
+
+    @UseGuards(JwtGuard, RolesGuard)
+    @Get(':id')
+    getAttemptById(@Param('id') userId: number) {
+        return this.attemptsService.getAttempt(userId);
+    }
+
+    @UseGuards(JwtGuard, RolesGuard)
+    @Patch(':id')
+    changeStatus(
+        @Param('id', ParseIntPipe) attemptId: number, 
+        @Body('status') attemptStatus: AttemptStatus
+    ) {
+        return this.attemptsService.changeStatus(attemptId, attemptStatus)
     }
 }
