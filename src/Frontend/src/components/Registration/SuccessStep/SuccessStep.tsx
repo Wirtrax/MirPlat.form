65c4ee4f224f1a35c7e8aa71ff877932be775ae0
millipo <palina517@gmail.com>
2026-09-01 19:07:29 +0300

    fix: registration

--- src/Frontend/src/components/Registration/SuccessStep/SuccessStep.tsx
+++ src/Frontend/src/components/Registration/SuccessStep/SuccessStep.tsx
@@ -1,15 +1,17 @@
 import { Link, useNavigate } from 'react-router-dom';
-
+import { useAppDispatch } from '../../../hooks/redux';
+import { fetchUser } from '../../../service/features/user/userSlice';
 import { ROUTES } from '../../../routes/routes';
 
 import ResultStep from '../../UI/ResultStep/ResultStep';
 
-
 export default function SuccessStep() {
   const navigate = useNavigate();
+  const dispatch = useAppDispatch();
 
-  const handleContinue = async () => {
-    navigate(ROUTES.INSTRUCTION);
+  const goNext = async (route: string) => {
+    await dispatch(fetchUser()).unwrap();
+    navigate(route);
   };
 
   return (
@@ -17,14 +19,17 @@ export default function SuccessStep() {
       title="Ты в игре!"
       description="Чтобы приложение было всегда под рукой, добавь его на экран телефона"
       buttonText="ДОБАВИТЬ НА ЭКРАН"
-      onButtonClick={handleContinue}>
+      onButtonClick={() => goNext(ROUTES.INSTRUCTION)}
+    >
       <Link
         to={ROUTES.HOME}
-        onClick={async (e) => {
+        onClick={(e) => {
           e.preventDefault();
-          navigate(ROUTES.HOME);
+          goNext(ROUTES.HOME);
         }}
-      >ПРОПУСТИТЬ</Link>
+      >
+        ПРОПУСТИТЬ
+      </Link>
     </ResultStep>
   );
 }
